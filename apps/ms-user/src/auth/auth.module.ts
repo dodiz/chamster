3b268f315c1743f743ff4c3f0d18bbf5192136e5
Donato Di Zenzo <d.dizenzo@be-tse.it>
2022-11-15 12:17:03 +0100

    added mongoose

--- apps/ms-user/src/auth/auth.module.ts
+++ apps/ms-user/src/auth/auth.module.ts
@@ -1,8 +1,18 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { environment } from "../environments/environment";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 
 @Module({
+  imports: [
+    MongooseModule.forRoot(environment.DB_URL, {
+      auth: {
+        password: "admin",
+        username: "admin",
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
